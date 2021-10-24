0c0d92b3f0bcf8a4fcfe2c9cdedb44a6c3a67789
Arthur <santosalmeida.arthur@gmail.com>
2021-10-23 21:37:53 -0300

    quebrando o loop em dois e renomeando metodo

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ function statement(invoice, plays) {
     let volumeCredits = 0
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
-
+        
         // print line for this order
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
         totalAmount += amountFor(perf)
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`
+    let volumeCredits = 0
+    for(let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
+    }
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`
     result += `You earned ${volumeCredits} credits\n`
     return result
 }
@@ -47,7 +50,7 @@ function volumeCreditsFor(aPerformance) {
     return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat("en­US",
         {
             style: "currency", currency: "USD",
