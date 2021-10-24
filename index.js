5888bf83cc775ba175d1fcf628f5fb3e34bd7c36
Arthur <santosalmeida.arthur@gmail.com>
2021-10-23 21:30:50 -0300

    extraindo numberformat em um metodo

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 function statement(invoice, plays) {
     let totalAmount = 0
     let volumeCredits = 0
-    let result = `Statement for ${invoice.customer}\n`
-    const format = new Intl.NumberFormat("en­US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format
+    let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf)
 
@@ -25,13 +20,13 @@ function amountFor(aPerformance) {
         case "tragedy":
             result = 40000
             if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience -­ 30)
+                result += 1000 * (aPerformance.audience - 30)
             }
             break
         case "comedy":
             result = 30000
             if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience -­ 20)
+                result += 10000 + 500 * (aPerformance.audience - 20)
             }
             result += 300 * aPerformance.audience
             break
@@ -47,7 +42,15 @@ function playFor(aPerformance) {
 
 function volumeCreditsFor(aPerformance) {
     let result = 0
-    result += Math.max(aPerformance.audience -­ 30, 0)
+    result += Math.max(aPerformance.audience - 30, 0)
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
     return result;
+}
+
+function format(aNumber) {
+    return new Intl.NumberFormat("en­US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber)
 }
